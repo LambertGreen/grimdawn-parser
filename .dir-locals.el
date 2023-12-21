175ff219b929599faf49b8833fe7aaf31e4409c3
Lambert Green <lambert_green@msn.com>
2023-12-21 14:34:01 -0800

    Add Emacs directory locals file

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode . ((flycheck-clang-standard-library . "c++17")
+              (flycheck-clang-language-standard . "c++17")
+              (compile-command . "just build"))))
