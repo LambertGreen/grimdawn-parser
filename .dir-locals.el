f2673a1263c1345e29fde80087ca8136ca7f3e6f
Lambert Green <lambert_green@msn.com>
2023-12-21 18:10:28 -0800

    Remove incorrect standard library value

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,7 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((flycheck-clang-standard-library . "c++17")
-              (flycheck-clang-language-standard . "c++17")
+((c++-mode . ((flycheck-clang-language-standard . "c++17")
               (projectile-project-compilation-cmd . "just build")
               (projectile-project-test-cmd . "just test")
               (compile-command . "just build"))))
